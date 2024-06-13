e7afcef98a51fd30f9af02869db123539648579e
Avishi2511 <avishimittal2004@gmail.com>
2024-06-13 15:55:34 +0530

    nested_tags

--- main2.py
+++ main2.py
@@ -48,3 +48,30 @@ df = pd.DataFrame({"Product Name":product_name, "Prices":prices_list, "Descripti
 print(df)
 
 df.to_csv("product_details.csv")
+
+
+'''
+#extract data from nested HTML tags
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text, "lxml")
+
+#boxes = soup.find_all("div", class_ = "col-md-4 col-xl-4 col-lg-4")
+#print(len(boxes))
+
+#box = soup.find_all("div", class_ = "col-md-4 col-xl-4 col-lg-4")[2]
+#print(box)
+#name = box.find("a").text
+#print(name)
+#desc = box.find("p", class_ = "description").text
+#print(desc)
+
+navbar = soup.find_all("ul", class_ = "nav", id="side-menu")[0]
+text = navbar.find("li", class_ = "active")
+print(text.text)
+
+'''
